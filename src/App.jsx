4777bbe5e1778b9dda61e8cd271b77b83133397e
JunuThapa <junuthapa011@gmail.com>
2023-05-26 00:38:27 -0400

    added map array iteration, html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const person = [
+    {
+      "name": "Alice",
+      "age": 25,
+      "hobbies": ["Reading", "Gardening", "Cooking"],
+      "bio": "Alice is a passionate learner who enjoys exploring new technologies."
+    },
+    {
+      "name": "Bob",
+      "age": 27,
+      "hobbies": ["Playing guitar", "Hiking", "Photography"],
+      "bio": "Bob is a creative individual who loves expressing himself through music and capturing moments with his camera."
+    },
+    {
+      "name": "Charlie",
+      "age": 23,
+      "hobbies": ["Painting", "Writing", "Soccer"],
+      "bio": "Charlie is an artist at heart, always seeking inspiration from various forms of art."
+    },
+    {
+      "name": "David",
+      "age": 29,
+      "hobbies": ["Swimming", "Traveling", "Programming"],
+      "bio": "David is an adventurer who is passionate about coding and exploring the world at the same time."
+    }
+  ] 
+
+  const person_array = person.map(function (arr){
+    return (<>
+    <div className = "person">
+    <h1>{arr.name}</h1>
+    <h2>{arr.age}</h2>
+    {arr.hobbies.map(function(hobby){return (<><li>{hobby}</li> </>)})}
+    <p>{arr.bio}</p>
+    </div>
+    </>)
+  })
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main">
+      <header>Know Our Experts</header>
+      <div className="array">
+        {person_array}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
